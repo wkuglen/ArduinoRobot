e22770cd88cc0a268598a0ce087252c039d27c86
Matthew Webb <matthewwebb65@yahoo.com>
2015-03-09 08:01:46 -0500

    fixed left stop constant

--- RobotBase/RobotBase.ino
+++ RobotBase/RobotBase.ino
@@ -20,11 +20,12 @@ int RIGHT_STOP = 92;
 int RIGHT_FORWARD_MAX = RIGHT_STOP - 50;
 int RIGHT_BACKWARD_MAX = RIGHT_STOP + 50;
 
-int LEFT_STOP = 82;
+int LEFT_STOP = 81;
 int LEFT_FORWARD_MAX = LEFT_STOP + 50;
 int LEFT_BACKWARD_MAX = LEFT_STOP - 50;
 
 void setup() {
+  Serial.begin(9600);
   //servos
   rightServo.attach(rightServoPin);
   leftServo.attach(leftServoPin);
@@ -39,6 +40,10 @@ void setup() {
 void loop() {
   leftLight = analogRead(leftPhotoPin);
   rightLight = analogRead(rightPhotoPin);
-  rightServo.write(RIGHT_FORWARD_MAX);
-  leftServo.write(LEFT_FORWARD_MAX);
+  //rightServo.write(RIGHT_FORWARD_MAX);
+  //leftServo.write(LEFT_FORWARD_MAX);
+  
+  Serial.println("~");
+  Serial.println(leftLight);
+  Serial.println(rightLight);
 }
