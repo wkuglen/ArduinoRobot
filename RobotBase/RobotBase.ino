0abe1e8a3bca4203afd65ffffda22a32336696b9
Matthew Webb <matthewwebb65@yahoo.com>
2015-03-11 07:38:10 -0500

    added 4 possible cases for photoresistors

--- RobotBase/RobotBase.ino
+++ RobotBase/RobotBase.ino
@@ -17,6 +17,8 @@ int rightLight;
 boolean leftOn;
 boolean rightOn;
 
+long commandStart;
+
 //left drive constants
 int RIGHT_STOP = 92;
 int RIGHT_FORWARD_MAX = RIGHT_STOP - 50;
@@ -28,9 +30,11 @@ int LEFT_BACKWARD_MAX = LEFT_STOP - 50;
 
 void setup() {
   Serial.begin(9600);
+  
   //servos
   rightServo.attach(rightServoPin);
   leftServo.attach(leftServoPin);
+  
   //button
   pinMode(buttonPin, INPUT);
   while(digitalRead(buttonPin) != HIGH) {
@@ -43,13 +47,25 @@ void loop() {
   //input
   leftLight = analogRead(leftPhotoPin);
   rightLight = analogRead(rightPhotoPin);
+
+  //process
   leftOn = leftLight < 43;
   rightOn = rightLight < 18;
+  if (rightOn && leftOn) {
+    //go straight
+  } else if (!rightOn && !leftOn) {
+    //uh oh
+  } else if (rightOn && !leftOn) {
+    //turn right
+  } else if (!rightOn && leftOn) {
+    //turn left
+  }
 
-  //prin
+  //print
   Serial.println("~");
   Serial.println(leftOn);
   Serial.println(rightOn);
   delay(500);
 }
+
 
