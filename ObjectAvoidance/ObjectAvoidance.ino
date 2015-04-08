f7e5310c7d8ca1eb6bfda77397f13c329d352333
Matthew Webb <matthewwebb65@yahoo.com>
2015-04-08 12:22:43 -0500

    moved around folders so that IDE doesn't complain

--- ObjectAvoidance/ObjectAvoidance.ino
+++ ObjectAvoidance/ObjectAvoidance.ino
@@ -3,6 +3,7 @@
 //declarations
 Servo rightServo;
 Servo leftServo;
+int cyclesLeft;//1 cycle = 50 ms
 
 //pins
 int rightServoPin = 5;
@@ -27,17 +28,18 @@ int LEFT_FORWARD_MAX = LEFT_STOP + 10;
 int LEFT_BACKWARD_MAX = LEFT_STOP - 10;
 
 void setup() {
+  Serial.begin(9600);
+  
   //servos
   rightServo.attach(rightServoPin);
   leftServo.attach(leftServoPin);
-  
+
   //button
   pinMode(buttonPin, INPUT);
-  while(digitalRead(buttonPin) != HIGH) {
-    //Rewrites Stop values to current darkness on track
+  while (digitalRead(buttonPin) != HIGH) {
     rightServo.write(RIGHT_STOP);
     leftServo.write(LEFT_STOP);
-  }//WHILE
+  }//while
 }
 
 void loop() {
@@ -45,7 +47,12 @@ void loop() {
   leftBumper = digitalRead(leftBumperPin);
   centerBumper = digitalRead(centerBumperPin);
   rightBumper = digitalRead(rightBumperPin);
-  
+  Serial.print(leftBumper);
+  Serial.print(" ");
+  Serial.print(centerBumper);
+  Serial.print(" ");
+  Serial.println(rightBumper);
+
   //process
   if (!leftBumper && centerBumper && !rightBumper) {
     //hit small object, turn somewhere
@@ -60,11 +67,12 @@ void loop() {
   } else if (!leftBumper && centerBumper && rightBumper) {
     //hit right set, turn left
   } else {
-    //hit nothing, go straight
+    rightServo.write(RIGHT_FORWARD_MAX);
+    leftServo.write(LEFT_FORWARD_MAX);
   }
-  
+
   //delay
-  delay(100);//Small delay to account for change
+  delay(.05 * 1000);
 }
 
 
